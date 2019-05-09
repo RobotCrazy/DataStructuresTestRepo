305264530c5a2d983d9eb6c9f1f7a45c2b290674
Benjamin <benjaminjacob12@gmail.com>
2019-05-09 09:27:05 -0400

    fixed Upneet's bad code

--- GitPracticeProject/src/GitPracticeClass.java
+++ GitPracticeProject/src/GitPracticeClass.java
@@ -1,32 +1,29 @@
 import java.util.Scanner;
 
-public class GitPracticeClass {
-//	public static void main(String[] args) {
-//		average();
-//	}
-	
-	public static void average() {
-		System..println("Enter five numbers:");
-		Scanner s = new Scanner(System.in);
-		
-		int num1 = Integer.parseInt(s.next());
-		 num2 = Integer.parseInt(s.next());
-		int num3 = Integer.parseInt(s.next());
-		int num4 = Integer.parseInt(s.next());
-		int num5 = Integer.parsInt(s.next());
-		
-		System.out.println("\nAverage:");
-		System.out.println((num1 + num2 + nu + num4 + num5) / 5);
-	}
-	
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		System.out.println("Type one integer");
-		int a = s.nextInt();
-		System.out.println("Type another integer");		
-		int b = s.nextInt();
-		int avg = (a + b) / 2; 
-		System.out.println("This is the average of the two numbers: " + avg);
-	}
-	
+public class GitPracticeClass
+{
+    // public static void main(String[] args) {
+    // average();
+    // }
+    private static Scanner scan = new Scanner( System.in );
+
+    public static void average()
+    {
+        System.out.println( "Enter five numbers, add spaces between:" );
+
+        int num1 = Integer.parseInt( scan.next() );
+        int num2 = Integer.parseInt( scan.next() );
+        int num3 = Integer.parseInt( scan.next() );
+        int num4 = Integer.parseInt( scan.next() );
+        int num5 = Integer.parseInt( scan.next() );
+
+        System.out.println( "\nAverage:" );
+        System.out.println( (num1 + num2 + num3 + num4 + num5) / 5 );
+    }
+
+    public static void main( String[] args )
+    {
+        average();
+    }
+
 }
